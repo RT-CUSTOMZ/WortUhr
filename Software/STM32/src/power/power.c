4df35f4523a67405a3cf0f3dd352cf458c04b6f9
Dennis Ahrens <dennis.ahrens@gmx.net>
2019-01-03 23:44:43 +0100

    -sort power pins -turn off leds after enable of each power pin

--- Software/STM32/src/power/power.c
+++ Software/STM32/src/power/power.c
@@ -19,12 +19,15 @@
 #include "io.h"
 #include "log.h"
 #include "delay.h"
+#include "wclock24h-config.h"
+#include "display.h"
 
 #if defined (STM32F4XX)                                         // STM32F4xx Nucleo Board PC8
 #define POWER_PERIPH_CLOCK_CMD  RCC_AHB1PeriphClockCmd
 #define POWER_PERIPH            RCC_AHB1Periph_GPIOC
 #define POWER_PORT              GPIOC
 #define POWER_PIN               GPIO_Pin_8
+#define POWER_PIN_ALL           POWER_PIN
 
 #elif defined (STM32F103)
 #define POWER_PERIPH_CLOCK_CMD  RCC_APB2PeriphClockCmd          // STM32F103 PB0
@@ -33,10 +36,10 @@
 //#define POWER_PIN               GPIO_Pin_0
 #define POWER_PIN1              GPIO_Pin_3
 #define POWER_PIN2              GPIO_Pin_4
-#define POWER_PIN3              GPIO_Pin_12
+#define POWER_PIN3              GPIO_Pin_15
 #define POWER_PIN4              GPIO_Pin_13
-#define POWER_PIN5              GPIO_Pin_14
-#define POWER_PIN6              GPIO_Pin_15
+#define POWER_PIN5              GPIO_Pin_12
+#define POWER_PIN6              GPIO_Pin_14
 #define POWER_PIN_ALL           POWER_PIN1|POWER_PIN2|POWER_PIN3|POWER_PIN4|POWER_PIN5|POWER_PIN6
 
 #else
@@ -62,7 +65,9 @@ power_init (void)
     GPIO_StructInit (&gpio);
     POWER_PERIPH_CLOCK_CMD (POWER_PERIPH, ENABLE);     // enable clock for power port
 
-    gpio.GPIO_Pin   = POWER_PIN_ALL;//POWER_PIN;
+    GPIO_RESET_BIT(POWER_PORT, POWER_PIN_ALL);  //set output register low before init
+
+    gpio.GPIO_Pin   = POWER_PIN_ALL;
     gpio.GPIO_Speed = GPIO_Speed_2MHz;
 
 #if defined (STM32F10X)
@@ -92,17 +97,23 @@ power_on (void)
         //step by step with delay
         delay_init(0);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN1);
+        led_refresh(DSP_DISPLAY_LED_OFFSET+3*WC_ROWS);
         delay_msec(POWER_ON_DELAY);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN2);
+        led_refresh(DSP_DISPLAY_LED_OFFSET+6*WC_ROWS);
         delay_msec(POWER_ON_DELAY);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN3);
+        led_refresh(DSP_DISPLAY_LED_OFFSET+9*WC_ROWS);
         delay_msec(POWER_ON_DELAY);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN4);
+        led_refresh(DSP_DISPLAY_LED_OFFSET+13*WC_ROWS);
         delay_msec(POWER_ON_DELAY);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN5);
+        led_refresh(DSP_DISPLAY_LED_OFFSET+16*WC_ROWS);
         delay_msec(POWER_ON_DELAY);
         GPIO_SET_BIT(POWER_PORT, POWER_PIN6);
-		
+        led_refresh(DSP_DISPLAY_LED_OFFSET+19*WC_ROWS);
+
         log_message ("switching power on");
         power.is_on = 1;
     }
